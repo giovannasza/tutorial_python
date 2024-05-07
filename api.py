1a826e4f5f7e51de5482e94b38ec4429a57236b5
Giovanna <giovannasouza1108@gmail.com>
2024-05-07 12:53:23 -0300

    tudo salvo

--- api.py
+++ api.py
@@ -11,21 +11,21 @@ def aleatorios():
     a = random.randint(49, 100)
     return jsonify({"status": 200, "data": a})
 
-@bp.route("/argumentos/<string:nome>")
+@bp.route("/api/argumentos/<string:nome>")
 def argumentos(nome: str):
     return jsonify({"status": 200, "data": nome})
 # Dados das pessoas
 
 
 # Rota para contar pessoas com mais de 50 anos
-@bp.route('/A', methods=['GET'])
+@bp.route('/api/A', methods=['GET'])
 def contar_pessoas_maiores_de_50_route():
     contador = maior_de_50(pessoas)
     return jsonify({'quantidade': contador})
 
 
 # Rota para contar pessoas que ganham mais de R$ 2000 e calcular a porcentagem
-@bp.route('/B', methods=['GET'])
+@bp.route('/api/B', methods=['GET'])
 def contar_pessoas_mais_de_2000_route():
     count, porcentagem, total_registros = mais_2000(pessoas)
     return jsonify({'quantidade': count, 'porcentagem': porcentagem, 'total_registros': total_registros})
@@ -37,7 +37,7 @@ def salario_mais_baixo(lista):
     return min(pessoa['salario'] for pessoa in lista)
 
 # Rota para retornar as 3 pessoas com maiores salários
-@bp.route('/C', methods=['GET'])
+@bp.route('/api/C', methods=['GET'])
 def obter_tres_maiores_salarios():
     tres_maiores_salarios = []
     for _ in range(3):
@@ -47,13 +47,13 @@ def obter_tres_maiores_salarios():
     return jsonify(tres_maiores_salarios)
 
 # Rota para retornar a média salarial de cada profissão
-@bp.route('/D', methods=['GET'])
+@bp.route('/api/D', methods=['GET'])
 def calcular_media_salarial_por_profissao():
     medias = media_profissoes(pessoas)
     return jsonify(medias)
 
 # Rota para retornar o intervalo da maioria das idades e o sexo de quem ganha mais de R$ 2000
-@bp.route('/E', methods=['GET'])
+@bp.route('/api/E', methods=['GET'])
 def calcular_intervalo_idades_e_sexo():
     intervalo_idades, sexo = maior_2000_sexo(pessoas)
     return jsonify({'intervalo_idades': intervalo_idades, 'sexo': sexo})
